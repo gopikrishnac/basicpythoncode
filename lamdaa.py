3089276826e7d2289bd5fff4e56725f693248de0
Gopikrishna <gopikrishnachodapaneedi@gmail.com>
2022-07-03 12:22:18 +0530

    code refactoring

--- lamdaa.py
+++ lamdaa.py
@@ -1,8 +1,8 @@
 from functools import reduce
-num=[2,5,6,8,7,14,66]
-evens=list(filter(lambda a : a%2==0,num))
-doubles=list(map(lambda a : a*2,evens))
-sum=reduce(lambda a,b:a+b,doubles )
+num = [2, 5, 6, 8, 7, 14, 66]
+evens = list(filter(lambda a: a % 2 == 0, num))
+doubles = list(map(lambda a: a*2, evens))
+sum = reduce(lambda a, b: a+b, doubles)
 print(evens)
 print(doubles)
-print(sum)
+print("sum", sum)
